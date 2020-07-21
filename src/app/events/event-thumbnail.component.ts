9f9fce28dba585c84e1d4bc35da8bcee7deafaae
Janet Woods <janetnwoods@gmail.com>
2020-07-21 12:30:32 -0400

    added navheader shell

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -3,17 +3,21 @@ import { Component, Input} from '@angular/core'
 @Component({
   selector: 'event-thumbnail',
   template: `
-    <div class="well hoverwell thumbnail">
+  <div class="well hoverwell thumbnail">
     <h2>{{event.name}}</h2>
     <div>Date: {{event.date}}</div>
     <div>{{event.time}}</div>
     <div>Price: \${{ event.price }}</div>
     <span>Location: {{event.location.address}}</span>
-    <span> &nbsp; </span>
-    <span>{{event.location.city}}, {{event.location.country}}</span>
+
+    <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
   </div>
-`
+`,
+  styles: [`
+    .pad-left {margin-left: 40px;}
+    `]
 })
 export class EventThumbnailComponent {
-  @Input() event:any
+  @Input() event: any
+  someProperty: any = "some value"
 }
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core'
 <div>
 <h1>Upcoming Angular Events</h1>
 <hr/>
-<event-thumbnail [event]="event1"></event-thumbnail>
+<div class ="well">
+<h2>Hello there!</h2>
+</div>
+
+<event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
 </div>`
 })
 
@@ -24,5 +28,5 @@ export class EventsListComponent {
       country: 'England'
     }
   }
-
+ 
 }
