2ca853209d180917b427d371563d57c64d45d2fb
Janet Woods <janetnwoods@gmail.com>
2020-07-23 11:14:42 -0400

    routing working again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
 import { EventService } from './events/shared/event.service'
-
+import { EventDetailsComponent } from './events/event-details/event-details.component'
+import { appRoutes } from './routes'
+import { RouterModule } from '@angular/router'
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
