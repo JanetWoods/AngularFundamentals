7f4198c67242d069669bede7ccc0491205b51675
Janet Woods <janetnwoods@gmail.com>
2020-07-23 09:48:34 -0400

    switch statement added to time

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,12 @@ import { Component, Input} from '@angular/core'
   <div class="well hoverwell thumbnail">
     <h2>{{event.name}}</h2>
     <div>Date: {{event.date}}</div>
-    <div>{{event.time}}</div>
+    <div [ngSwitch]="event?.time">{{event.time}}
+      <span *ngSwitchCase="'8:00 am'"> (Early Start)</span>
+      <span *ngSwitchCase="'10:00 am'"> (Late Start)</span>
+      <span *ngSwitchDefault> (Normal Start)</span>
+    </div>
+
     <div>Price: \${{ event?.price }}</div>
     <div *ngIf="event?.location">
     <span>Location: {{event?.location?.address}}</span>
@@ -20,8 +25,7 @@ import { Component, Input} from '@angular/core'
   styles: [`
     .thumbnail {min-height: 210px;}
     .pad-left {margin-left: 10px;}
-    .well div { color: #bbb;}
-    `]
+     `]
 })
 export class EventThumbnailComponent {
   @Input() event: any
