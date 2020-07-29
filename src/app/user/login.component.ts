fb0ce07334867bf6fde3ecf6e2ca85c872a9d09c
Janet Woods <janetnwoods@gmail.com>
2020-07-29 17:23:27 -0400

    more form validation

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,13 +4,17 @@ import { Router } from '@angular/router'
 
 
 @Component({
-  templateUrl: './login.component.html'
-})
+  templateUrl: './login.component.html',
+  styles: [`
+    em{ float:right; color:#E05c65; padding-left:10px;}
+    `]  
+  })
 
 export class LoginComponent {
   userName
   password
   firstName
+  mouseoverLogin
 
   constructor(private authService: AuthService, private router:Router) { }
 
