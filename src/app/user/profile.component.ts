38c3f7694ca7cb298568c864caa97246f95fc523
Janet Woods <janetnwoods@gmail.com>
2020-08-03 14:31:10 -0400

    both names validate properly now

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -24,8 +24,9 @@ export class ProfileComponent implements OnInit {
   ngOnInit() {
     this.firstName = new FormControl
       (this.authService.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')])
-    this.lastName = new FormControl
-      (this.authService.currentUser.lastName, Validators.required)
+
+      this.lastName = new FormControl
+      (this.authService.currentUser.lastName, [Validators.required, Validators.pattern('[a-zA-Z].*')])
 
     this.profileForm = new FormGroup({
       firstName: this.firstName,
