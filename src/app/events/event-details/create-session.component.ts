7853ca35f530722291eb70ce6c8547f0af9f7588
Janet Woods <janetnwoods@gmail.com>
2020-08-06 15:09:01 -0400

    validating new session form values without styling

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core'
-import {FormControl, FormGroup, Validators, Form} from '@angular/forms'
+import {FormControl, FormGroup, Validators} from '@angular/forms'
 import {ISession} from '../shared/index'
 
 
