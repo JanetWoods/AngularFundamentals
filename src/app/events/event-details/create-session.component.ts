9c17f181e99be25b9b30ef817e2573164273d693
Janet Woods <janetnwoods@gmail.com>
2020-08-06 17:48:00 -0400

    added restrictedWords custom validator

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit} from '@angular/core'
 import {FormControl, FormGroup, Validators} from '@angular/forms'
-import {ISession} from '../shared/index'
-
+import {ISession, restrictedWords} from '../shared/index'
 
 @Component({
   templateUrl: './create-session.component.html',
@@ -28,7 +27,7 @@ export class CreateSessionComponent implements OnInit{
       this.presenter = new FormControl('', Validators.required)
       this.duration = new FormControl('', Validators.required)
       this.level = new FormControl('', Validators.required)
-      this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)])
+      this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo', 'bar']) ])
 
       this.newSessionForm = new FormGroup({
         name: this.name,
@@ -38,12 +37,7 @@ export class CreateSessionComponent implements OnInit{
         abstract: this.abstract
       })
   }
-  private restrictedWords(control: FormControl): { [key: string]: any }
-  {
-    return control.value.includes('foo')
-      ? { 'restrictedWords': 'foo' }
-      : null
-  }
+   
 
   saveSession(formValues) {
     let session:ISession = {
