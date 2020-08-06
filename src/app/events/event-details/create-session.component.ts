154bff49486acfa48dfbeec8a1c5c6c1f33ae2b7
Janet Woods <janetnwoods@gmail.com>
2020-08-06 15:19:24 -0400

    validating new session form with error styling

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -4,7 +4,15 @@ import {ISession} from '../shared/index'
 
 
 @Component({
-  templateUrl: './create-session.component.html'
+  templateUrl: './create-session.component.html',
+  styles: [`
+   em { float: right; color:#E05C65; padding- left: 10px;}
+  .error input, .error select, .error textarea { background-color:#E3C3C5;}
+  .error ::-webkit-input - Placeholder { color: #999; }
+  .error ::-moz-placeholder{ color: #999; }
+  .error :-moz-placeholder{color: #999;}
+  .error :ms-input-placeholder{color: #999;}
+`]
 })
 export class CreateSessionComponent implements OnInit{
 
@@ -41,5 +49,8 @@ export class CreateSessionComponent implements OnInit{
       voters:[]
     }
     console.log(formValues)
+  }
+  cancelSession() {
+
   }
 }
